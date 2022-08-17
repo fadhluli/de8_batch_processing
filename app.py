36d7f2dbb62c9495a812187794520414e3ba73d7
affad <59091083+fadhluli@users.noreply.github.com>
2022-08-17 15:33:10 +0700

    create app

---
+++ app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import os
+import json
+import psycopg2
+
+if __name__ == '__main__':
+    path = os.getcwd()
+    with open(path+'/'+'config.json') as file:
+        conf = json.load(file)['postgresql']
+
+    try:
+        conn = psycopg2.connect(host=conf['host'], database=conf['db'],
+                                user=conf['user'], password=conf['pwd'])
+        print("[INFO] Success connecting PostgreSQL....")
+    except:
+        print("[INFO] Can't connect PostgreSQL....")
